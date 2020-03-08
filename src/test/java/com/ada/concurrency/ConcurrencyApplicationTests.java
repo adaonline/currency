8f5e9e05b90872aba9e241c0da7e0417d0d1e8ac
adaonline <adaonline@126.com>
2020-03-09 00:19:16 +0800

    init

---
+++ src/test/java/com/ada/concurrency/ConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ada.concurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
